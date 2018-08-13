8bfb8a56d82cbe41aacdfb2a77b2d2626d5acfe8
HenryHill505 <hhill505@gmail.com>
2018-08-13 14:50:24 -0500

    Wrote RequestManager.GetPictureOfTheDay()

---
+++ Stargazer/RequestManager.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using Stargazer.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Stargazer
+{
+    public class RequestManager
+    {
+        public async Task<string> GetPictureOfTheDay()
+        {
+            string url = "https://api.nasa.gov/planetary/apod?api_key=" + Keyring.NASAKey;
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(responseBody);
+            string picurl = (string)json["url"];
+            return picurl;
+        }
+    }
+}
