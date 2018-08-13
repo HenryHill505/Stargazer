cf23d9846001f8c81d7a905be388c08512f057ed
HenryHill505 <hhill505@gmail.com>
2018-08-13 13:51:44 -0500

    Created Keyring.cs

---
+++ Stargazer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stargazer.Startup))]
+namespace Stargazer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
