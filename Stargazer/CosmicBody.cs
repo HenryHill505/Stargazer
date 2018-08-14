2bd83a32537c298964f3f94b30d1fb09bff8f056
HenryHill505 <hhill505@gmail.com>
2018-08-14 15:18:43 -0500

    Created class CosmicBody and Comet which inherits from it

---
+++ Stargazer/CosmicBody.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stargazer
+{
+    public class CosmicBody
+    {
+        public int magnitude;
+        public string name;
+    }
+}
--- Stargazer/RequestManager.cs
+++ Stargazer/RequestManager.cs
@@ -12,6 +12,7 @@ namespace Stargazer
     public static class RequestManager
     {
         static string PictureOfTheDayService = "https://api.nasa.gov/planetary/apod?api_key=" + Keyring.NASAKey;
+        static string NearEarthCometsService = "https://data.nasa.gov/resource/2vr3-k9wn.json";
         public static async Task<JObject> GetPictureOfTheDayJSON()
         {
             HttpClient client = new HttpClient();
@@ -26,6 +27,15 @@ namespace Stargazer
         {
             JObject json = GetPictureOfTheDayJSON().Result;
             return (string)json["url"];
+        }
+
+        public static async Task<JObject> GetCometJSON()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(NearEarthCometsService).ConfigureAwait(false);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(responseBody);
+            return json;
         }
     }
 }
