f1d408088b98ad0eadc16b9b0aad617a96654935
HenryHill505 <hhill505@gmail.com>
2018-08-15 07:48:55 -0500

    Created SearchController

--- Stargazer/Startup.cs
+++ Stargazer/Startup.cs
@@ -9,9 +9,6 @@ namespace Stargazer
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-
-            RequestManager.GetCometList();
-
         }
     }
 }
